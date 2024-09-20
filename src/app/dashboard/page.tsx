02f69dca2f2e6270193d59a02a6a6fc25652e0ef
kenzycodex <kenzycodex@gmail.com>
2024-09-20 16:00:02 +0100

    Fixed the import module error in src/app/dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 // src/app/dashboard/page.tsx
-import Dashboard from "@/components/dashboard/dashboard";
+import Dashboard from "@/components/Dashboard/Dashboard";
 
 export default function DashboardPage() {
   return <Dashboard />;
