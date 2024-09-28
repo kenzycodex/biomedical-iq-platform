a6c04355083255afe928f4b5013df106ca20f089
kenzycodex <kenzycodex@gmail.com>
2024-09-28 02:45:30 +0100

    CODE, CODE, CODE

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { ReactNode } from "react";
-import MainHeader from "@/components/Header/MainHeader"; // Ensure the import matches the component name
+import MainHeader from "@/components/Header/MainHeader";
 
 export default function MainLayout({
   children,
@@ -8,26 +8,16 @@ export default function MainLayout({
   children: ReactNode;
 }) {
   return (
-    <>
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className="flex">
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col lg:ml-72.5">
-          {/* <!-- ===== Header Start ===== --> */}
-          <MainHeader /> {/* Use the component with PascalCase */}
-          {/* <!-- ===== Header End ===== --> */}
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <MainHeader />
 
-          {/* <!-- ===== Main Content Start ===== --> */}
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
+      {/* Main Content */}
+      <main className="flex-grow flex items-center justify-center">
+        <div className="w-full max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          {children}
         </div>
-        {/* <!-- ===== Content Area End ===== --> */}
-      </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
-    </>
+      </main>
+    </div>
   );
 }
